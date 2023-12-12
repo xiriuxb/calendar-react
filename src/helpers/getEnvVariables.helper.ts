cca5dc962cbec6b7f46b3d58f1c26782fae1c11a
Jorge Trujillo <39315478+xiriuxb@users.noreply.github.com>
2023-12-12 12:12:43 -0500

    Update getEnvVariables.helper.ts

--- src/helpers/getEnvVariables.helper.ts
+++ src/helpers/getEnvVariables.helper.ts
@@ -2,6 +2,6 @@ export const getEnvVariables = () =>{
     // import.meta.env;
 
     return {
-        VITE_API_URL:import.meta.env.VITE_API_URL;
+        VITE_API_URL:import.meta.env.VITE_API_URL,
     }
 }
